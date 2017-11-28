c316c6aee4567430cbf214eb197b111b6db2db8d
Jamie Townsend <jamiehntownsend@gmail.com>
2017-11-28 14:27:19 +0000

    Add value of cost to Adam callback

--- autograd/misc/optimizers.py
+++ autograd/misc/optimizers.py
@@ -18,51 +18,29 @@ def unflatten_optimizer(optimize):
     wrapped version that handles trees of nested containers (lists/tuples/dicts)
     with arrays/scalars at the leaves."""
     @wraps(optimize)
-    def _optimize(grad, x0, callback=None, *args, **kwargs):
+    def _optimize(val_and_grad, x0, callback=None, *args, **kwargs):
         _x0, unflatten = flatten(x0)
-        _grad = lambda x, i: flatten(grad(unflatten(x), i))[0]
+        def _val_and_grad(x, i):
+            val, grad = val_and_grad(unflatten(x), i)
+            return val, flatten(grad)[0]
         if callback:
-            _callback = lambda x, i, g: callback(unflatten(x), i, unflatten(g))
+            _callback = lambda x, i, g, v: callback(unflatten(x), i, unflatten(g), v)
         else:
             _callback = None
-        return unflatten(optimize(_grad, _x0, _callback, *args, **kwargs))
+        return unflatten(optimize(_val_and_grad, _x0, _callback, *args, **kwargs))
 
     return _optimize
 
 @unflatten_optimizer
-def sgd(grad, x, callback=None, num_iters=200, step_size=0.1, mass=0.9):
-    """Stochastic gradient descent with momentum.
-    grad() must have signature grad(x, i), where i is the iteration number."""
-    velocity = np.zeros(len(x))
-    for i in range(num_iters):
-        g = grad(x, i)
-        if callback: callback(x, i, g)
-        velocity = mass * velocity - (1.0 - mass) * g
-        x = x + step_size * velocity
-    return x
-
-@unflatten_optimizer
-def rmsprop(grad, x, callback=None, num_iters=100,
-            step_size=0.1, gamma=0.9, eps=10**-8):
-    """Root mean squared prop: See Adagrad paper for details."""
-    avg_sq_grad = np.ones(len(x))
-    for i in range(num_iters):
-        g = grad(x, i)
-        if callback: callback(x, i, g)
-        avg_sq_grad = avg_sq_grad * gamma + g**2 * (1 - gamma)
-        x = x - step_size * g/(np.sqrt(avg_sq_grad) + eps)
-    return x
-
-@unflatten_optimizer
-def adam(grad, x, callback=None, num_iters=100,
+def adam(val_and_grad, x, callback=None, num_iters=100,
          step_size=0.001, b1=0.9, b2=0.999, eps=10**-8):
     """Adam as described in http://arxiv.org/pdf/1412.6980.pdf.
     It's basically RMSprop with momentum and some correction terms."""
     m = np.zeros(len(x))
     v = np.zeros(len(x))
     for i in range(num_iters):
-        g = grad(x, i)
-        if callback: callback(x, i, g)
+        val, g = val_and_grad(x, i)
+        if callback: callback(x, i, g, val)
         m = (1 - b1) * g      + b1 * m  # First  moment estimate.
         v = (1 - b2) * (g**2) + b2 * v  # Second moment estimate.
         mhat = m / (1 - b1**(i + 1))    # Bias correction.
