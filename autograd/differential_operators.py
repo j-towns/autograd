7fad8d0e9cbb4ff5b3778636fb7515d1fa719557
Jamie Townsend <jamiehntownsend@gmail.com>
2018-08-07 15:33:43 +0100

    Further minor simplifications

--- autograd/differential_operators.py
+++ autograd/differential_operators.py
@@ -22,11 +22,7 @@ def grad(fun, x):
     positional argument number `argnum`. The returned function takes the same
     arguments as `fun`, but returns the gradient instead. The function `fun`
     should be scalar-valued. The gradient has the same type as the argument."""
-    vjp, ans = _make_vjp(fun, x)
-    if not vspace(ans).size == 1:
-        raise TypeError("Grad only applies to real scalar-output functions. "
-                        "Try jacobian, elementwise_grad or holomorphic_grad.")
-    return vjp(vspace(ans).ones())
+    return value_and_grad(fun)(x)[1]
 
 @unary_to_nary
 def elementwise_grad(fun, x):
@@ -127,18 +123,18 @@ def make_ggnvp(f, g=lambda x: 1./2*np.sum(x**2, axis=-1), f_argnum=0):
 def value_and_grad(fun, x):
     """Returns a function that returns both value and gradient. Suitable for use
     in scipy.optimize"""
-    return tuple(reversed(_grad_and_aux(lambda x: 2 * (fun(x),), x)))
+    return tuple(reversed(grad_and_aux(lambda x: 2 * (fun(x),))(x)))
 
-def _grad_and_aux(fun, x):
+@unary_to_nary
+def grad_and_aux(fun, x):
     """Builds a function that returns the gradient of the first output and the
     (unmodified) second output of a function that returns two outputs."""
-    vjp, (ans, aux) = _make_vjp(lambda x: atuple(fun(x)), x)
+    vjp, (ans, aux) = make_vjp(lambda x: atuple(fun(x)))(x)
     if not vspace(ans).size == 1:
-        raise TypeError("Gradient only applies to real scalar-output "
+        raise TypeError("Grad only applies to real scalar-output "
                         "functions. Try jacobian, elementwise_grad or "
                         "holomorphic_grad.")
     return vjp((vspace(ans).ones(), vspace(aux).zeros())), aux
-grad_and_aux = unary_to_nary(_grad_and_aux)
 
 def multigrad_dict(fun):
     "Takes gradients wrt all arguments simultaneously,"
